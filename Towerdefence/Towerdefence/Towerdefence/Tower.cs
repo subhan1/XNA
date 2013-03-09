aded0efb908d4325f1e579da4700c381a58886d1
subhan1 <subhanmahmood@hotmail.com>
2013-03-09 23:36:00 +0100

    xna project med DrawableGameComponent  alle klassene de fleste uten no inhold atm

---
+++ Towerdefence/Towerdefence/Towerdefence/Tower.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Towerdefence
+{
+    class Tower : GameObject
+    
+    {
+        Texture2D texture;
+        public Vector2 position;
+        private MouseState _currentState;
+        private MouseState _previousState;
+        public Rectangle rectangle;
+        
+      
+
+       
+
+
+
+        public void Update(GameTime gameTime)
+        {
+            _previousState = _currentState;
+            _currentState = Mouse.GetState();
+            
+                   
+            
+            if (_currentState.LeftButton == ButtonState.Pressed && _previousState.LeftButton == ButtonState.Released )
+            {
+                // Check if the mouse is clicked over the sprite 
+                if ((_currentState.X > position.X) && (_currentState.X < (position.X + texture.Width)) &&
+                    (_currentState.Y > position.Y) && (_currentState.Y < (position.Y + texture.Height)))
+                {
+                    position = new Vector2(50, 50);
+                    
+                }
+                
+                
+            }
+          
+        }
+
+           public bool IsButtonPressed()
+        {
+            if (_currentState.LeftButton == ButtonState.Pressed && _previousState.LeftButton == ButtonState.Released)
+                return true;
+            return false;
+        }
+
+           public bool IsButtonReleased()
+           {
+               if (_currentState.LeftButton == ButtonState.Pressed && _previousState.LeftButton == ButtonState.Pressed)
+                   return true;
+               return false;
+           }
+        
+      
+        
+
+    }
+}
